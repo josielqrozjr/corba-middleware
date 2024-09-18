344f53af8c032b6b80154178d423ac925b8468f9
Queiroz JR <131998280+josielqrozjr@users.noreply.github.com>
2024-09-18 12:16:31 -0300

    Update: interface CLI
    Criação de uma interface para um teste mais flexível

--- cliente/cliente.cpp
+++ cliente/cliente.cpp
@@ -13,6 +13,16 @@
 using namespace std;
 using namespace CORBA;
 
+void mostrar_menu() {
+    cout << "\n--- Menu ---\n";
+    cout << "1. Ver ID do objeto\n";
+    cout << "2. Inserir ou atualizar chave\n";
+    cout << "3. Consultar valor de uma chave\n";
+    cout << "4. Remover chave\n";
+    cout << "5. Sair\n";
+    cout << "Escolha uma opção: ";
+}
+
 int main(int argc, char* argv[])
 {
     try {
@@ -34,57 +44,58 @@ int main(int argc, char* argv[])
             return 1;
         }
     
-        // 3. Usa objeto (chama métodos)
+        int opcao = 0;
+        string chave, valor;
 
-        // Testa o método id()
-        char* object_id = etcd->id();
-        cout << "ID do objeto: " << object_id << endl;
-        CORBA::string_free(object_id);  // Libera memória alocada
+        while (opcao != 5) {
+            mostrar_menu();
+            cin >> opcao;
 
-        // Testa o método put()
-        cout << "Inserindo chave 'chave1' com valor 'valor1'..." << endl;
-        bool nova_chave = etcd->put("chave1", "valor1");
-        cout << (nova_chave ? "Nova chave inserida." : "Chave já existente, valor atualizado.") << endl;
-
-        // Testa o método get()
-        try {
-            char* valor = etcd->get("chave1");
-            cout << "Valor associado à chave 'chave1': " << valor << endl;
-            CORBA::string_free(valor);  // Libera memória alocada
-        } catch (EtcdModule::InvalidKey& e) {
-            cerr << "Erro: Chave 'chave1' não encontrada." << endl;
-        }
-
-        // Testa o método put() com atualização de valor
-        cout << "Atualizando chave 'chave1' com novo valor 'valor2'..." << endl;
-        nova_chave = etcd->put("chave1", "valor2");
-        cout << (nova_chave ? "Nova chave inserida." : "Chave já existente, valor atualizado.") << endl;
-
-        // Testa o método get() novamente após a atualização
-        try {
-            char* valor = etcd->get("chave1");
-            cout << "Valor atualizado associado à chave 'chave1': " << valor << endl;
-            CORBA::string_free(valor);
-        } catch (EtcdModule::InvalidKey& e) {
-            cerr << "Erro: Chave 'chave1' não encontrada." << endl;
-        }
-
-        // Testa o método del()
-        cout << "Removendo chave 'chave1'..." << endl;
-        try {
-            etcd->del("chave1");
-            cout << "Chave 'chave1' removida com sucesso." << endl;
-        } catch (EtcdModule::InvalidKey& e) {
-            cerr << "Erro: Chave 'chave1' não encontrada." << endl;
-        }
-
-        // Testa o método get() após a remoção (deve lançar exceção)
-        try {
-            char* valor = etcd->get("chave1");
-            cout << "Valor associado à chave 'chave1': " << valor << endl;
-            CORBA::string_free(valor);
-        } catch (EtcdModule::InvalidKey& e) {
-            cerr << "Erro: Chave 'chave1' foi removida, não encontrada." << endl;
+            switch(opcao) {
+                case 1: { // Ver ID do objeto
+                    char* object_id = etcd->id();
+                    cout << "ID do objeto: " << object_id << endl;
+                    CORBA::string_free(object_id);  // Libera memória alocada
+                    break;
+                }
+                case 2: { // Inserir ou atualizar chave
+                    cout << "Digite a chave: ";
+                    cin >> chave;
+                    cout << "Digite o valor: ";
+                    cin >> valor;
+                    bool nova_chave = etcd->put(chave.c_str(), valor.c_str());
+                    cout << (nova_chave ? "Nova chave inserida." : "Chave já existente, valor atualizado.") << endl;
+                    break;
+                }
+                case 3: { // Consultar valor de uma chave
+                    cout << "Digite a chave a ser consultada: ";
+                    cin >> chave;
+                    try {
+                        char* valor = etcd->get(chave.c_str());
+                        cout << "Valor associado à chave '" << chave << "': " << valor << endl;
+                        CORBA::string_free(valor);  // Libera memória alocada
+                    } catch (EtcdModule::InvalidKey& e) {
+                        cerr << "Erro: Chave '" << chave << "' não encontrada." << endl;
+                    }
+                    break;
+                }
+                case 4: { // Remover chave
+                    cout << "Digite a chave a ser removida: ";
+                    cin >> chave;
+                    try {
+                        etcd->del(chave.c_str());
+                        cout << "Chave '" << chave << "' removida com sucesso." << endl;
+                    } catch (EtcdModule::InvalidKey& e) {
+                        cerr << "Erro: Chave '" << chave << "' não encontrada." << endl;
+                    }
+                    break;
+                }
+                case 5: // Sair
+                    cout << "Encerrando o programa..." << endl;
+                    break;
+                default:
+                    cout << "Opção inválida. Tente novamente." << endl;
+            }
         }
 
 	    // 4. Finalizações
